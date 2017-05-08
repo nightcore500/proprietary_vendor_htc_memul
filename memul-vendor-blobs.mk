1b8c1a08a324689d681c013427fc23097ab1f1df
nightcore500 <nightcore500@users.noreply.github.com>
2017-05-08 18:40:23 +0200

    Update graphics blobs
    * 6.0.1 blobs from bacon (compiled from branch LA.BF.1.1.3_rb1.5)

--- memul-vendor-blobs.mk
+++ memul-vendor-blobs.mk
@@ -143,18 +143,18 @@ PRODUCT_COPY_FILES += \
         vendor/htc/memul/proprietary/vendor/lib/egl/libGLESv2_adreno.so:/system/vendor/lib/egl/libGLESv2_adreno.so \
         vendor/htc/memul/proprietary/vendor/lib/egl/libq3dtools_adreno.so:/system/vendor/lib/egl/libq3dtools_adreno.so \
         vendor/htc/memul/proprietary/vendor/lib/libadreno_utils.so:/system/vendor/lib/libadreno_utils.so \
+        vendor/htc/memul/proprietary/vendor/lib/libbccQTI.so:/system/vendor/lib/libbccQTI.so \
         vendor/htc/memul/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
         vendor/htc/memul/proprietary/vendor/lib/libc2d30-a3xx.so:/system/vendor/lib/libc2d30-a3xx.so \
-        vendor/htc/memul/proprietary/vendor/lib/libc2d30.so:/system/vendor/lib/libc2d30.so \
         vendor/htc/memul/proprietary/vendor/lib/libCB.so:/system/vendor/lib/libCB.so \
         vendor/htc/memul/proprietary/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
         vendor/htc/memul/proprietary/vendor/lib/libllvm-qcom.so:/system/vendor/lib/libllvm-qcom.so \
         vendor/htc/memul/proprietary/vendor/lib/libOpenCL.so:/system/vendor/lib/libOpenCL.so \
-        vendor/htc/memul/proprietary/vendor/lib/libOpenVG.so:/system/vendor/lib/libOpenVG.so \
         vendor/htc/memul/proprietary/vendor/lib/librs_adreno_sha1.so:/system/vendor/lib/librs_adreno_sha1.so \
         vendor/htc/memul/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
         vendor/htc/memul/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \
         vendor/htc/memul/proprietary/vendor/lib/libsc-a3xx.so:/system/vendor/lib/libsc-a3xx.so \
+        vendor/htc/memul/proprietary/vendor/lib/libscale.so:/system/vendor/lib/libscale.so \
         vendor/htc/memul/proprietary/etc/firmware/venus.b00:/system/etc/firmware/venus.b00 \
         vendor/htc/memul/proprietary/etc/firmware/venus.b01:/system/etc/firmware/venus.b01 \
         vendor/htc/memul/proprietary/etc/firmware/venus.b02:/system/etc/firmware/venus.b02 \
